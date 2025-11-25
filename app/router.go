e5eb57c1c69cc1e4dd8ddc0af0a01f8d04ce003d
factor <lukerbab@gmail.com>
2025-11-25 19:28:47 +0000

    feat: added in dynamic route matching

--- app/router.go
+++ app/router.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type Router interface {
@@ -12,6 +13,20 @@ type Router interface {
 }
 
 type router struct {
+}
+
+type routeHandler struct {
+	handler func()
+}
+
+type routeContext struct {
+	route string
+	path  string
+}
+
+type routePath struct {
+	handler func(context routeContext)
+	paths   map[string]routePath
 }
 
 func NewRouter() Router {
@@ -25,25 +40,41 @@ func (r router) handleRequest(conn net.Conn) error {
 }
 
 func (r router) route(conn net.Conn, header RequestHeader) {
-	route := header.getRoute()
+	requestedRoute := header.getRoute()
 
-	reg, _ := regexp.Compile("/([^/]+)?")
-
-	subMatch := reg.FindAllStringSubmatch(route, -1)
-	basePath := subMatch[0][0]
-
-	switch basePath {
-	case "/":
-		r.writeResponse(conn, 200, "OK", "test")
-		break
-	case "/echo":
-		subPath := subMatch[1][1]
-		r.writeResponse(conn, 200, "OK", subPath)
-		break
-	default:
-		r.writeResponse(conn, 404, "Not Found", "")
-		break
+	routes := map[string]routePath{
+		"/": {
+			handler: func(c routeContext) {
+				r.writeResponse(conn, 200, "OK", "test")
+			},
+		},
+		"/echo": {
+			handler: func(c routeContext) {
+				r.writeResponse(conn, 200, "OK", "test")
+			},
+			paths: map[string]routePath{
+				"/:value": {
+					handler: func(c routeContext) {
+						res := strings.Replace(c.path, "/", "", 1)
+						r.writeResponse(conn, 200, "OK", res)
+					},
+				},
+				"/bob": {
+					handler: func(c routeContext) {
+						r.writeResponse(conn, 200, "OK", "1")
+					},
+				},
+			},
+		},
+		"/404": {
+			handler: func(c routeContext) {
+				r.writeResponse(conn, 404, "Not Found", "")
+			},
+		},
 	}
+
+	rh := r.getHandler(routes, requestedRoute)
+	rh.handler()
 }
 
 func (r router) writeResponse(conn net.Conn, statusCode int, status string, body string) {
@@ -64,4 +95,42 @@ func (r router) parseRequest(conn net.Conn) RequestHeader {
 	}
 
 	return NewRequestHeader(requestHeader)
+}
+
+func (r router) getHandler(routes map[string]routePath, requestedRoute string) routeHandler {
+	regRoutePath := regexp.MustCompile(`/[^/]+|/`)
+	paths := regRoutePath.FindAllStringSubmatch(requestedRoute, -1)
+	selectedPath := paths[0][0]
+	selectedRoute := routes[paths[0][0]]
+
+	for i := 1; i < len(paths); i++ {
+		selectedPath = paths[i][0]
+		r := selectedRoute.paths[paths[i][0]]
+
+		if r.handler != nil {
+			selectedRoute = r
+		} else if selectedRoute.paths["/:value"].handler != nil {
+			selectedRoute = selectedRoute.paths["/:value"]
+		} else {
+			selectedRoute = routes["/404"]
+			selectedPath = "/404"
+		}
+	}
+
+	handler := selectedRoute.handler
+
+	if handler == nil {
+		handler = routes["/404"].handler
+	}
+
+	c := routeContext{
+		route: requestedRoute,
+		path:  selectedPath,
+	}
+
+	return routeHandler{
+		handler: func() {
+			handler(c)
+		},
+	}
 }
